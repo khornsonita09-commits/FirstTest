5bcec3e7d659cbf169c0a04ea8cc10ebbcf5a94a
MSII <shea6764@gmail.com>
2026-07-08 12:22:55 +0700

    This commit assignment on Option2

---
+++ Assignment.py
@@ -0,0 +1,14 @@
+#Options 2 The Border Pattern
+
+# Define the required variables
+first_name = "POV_MEAS"
+last_name = "Team"
+id_number = 1042
+
+# Create a variable for the border (exactly 25 asterisks)
+border = "*" * 28
+
+# Print the badge
+print(border)
+print(f"* VIP: {first_name} {last_name} ({id_number}) *")
+print(border)
