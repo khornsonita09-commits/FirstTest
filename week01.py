2ced0b5da40ee5b15450bd12e70b0589c9f2d633
MSII <shea6764@gmail.com>
2026-07-10 19:15:53 +0700

    This commit assignment on Option2

--- week01.py
+++ week01.py
@@ -1,5 +1,11 @@
+print("Your rate is : 7")
 hour = int(input("Please enter hour: "))
+
 rate = 7
+border = "=" * 30
 
 paid = hour * rate
-print(f"Paid :  $ {paid}")
+print(border)
+print(f"=       Paid: {hour} * {rate}         =")
+print(f"=      Result :  $ {paid}       =")
+print(border)
