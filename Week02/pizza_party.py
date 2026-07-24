cb296b6710cd61ee8f858a8e916ba3ec3c933cf3
MSII <shea6764@gmail.com>
2026-07-24 15:57:50 +0700

    This commit week2 New Directory

--- Week02/pizza_party.py
+++ Week02/pizza_party.py
@@ -3,4 +3,3 @@ friend = 6
 one_friend_get = slices // friend
 left_over = slices % friend
 print('one friend get:', one_friend_get, "\n", "left over:", left_over )
-
--- secondtomind.py
+++
@@ -1,5 +0,0 @@
-seconds = 500
-minutes = seconds // 60
-remaining_seconds = seconds % 60
-print(f"{seconds} seconds = {minutes} minutes and {remaining_seconds} seconds")
-
